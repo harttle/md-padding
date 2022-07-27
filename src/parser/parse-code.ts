97dd7519b30edfd4f1a809ff6adc8f9a4c75a352
Stephano George <huohuo5230@gmail.com>
2022-07-27 11:45:29 +0800

    Feat: comment support for go and sql

--- src/parser/parse-code.ts
+++ src/parser/parse-code.ts
@@ -5,13 +5,22 @@ type documentParser = (content: string) => Document
 
 const parsers = {
   cpp: cpp,
+  'c++': cpp,
   c: cpp,
   java: cpp,
   javascript: cpp,
+  js: cpp,
   csharp: cpp,
+  'c#': cpp,
   typescript: cpp,
+  ts: cpp,
+  go: cpp,
+  sql: sql,
   bash: bash,
+  shell: bash,
+  sh: bash,
   python: bash,
+  py: bash,
   ruby: bash
 }
 
@@ -29,6 +38,36 @@ function * cpp (code: string, parseMarkdown: documentParser) {
   while (i < N) {
     const c2 = code.substr(i, 2)
     if (c2 === '//') {
+      const j = code.indexOf('\n', i)
+      const end = j === -1 ? N : j
+      if (i + 2 > prevI) {
+        yield new Raw(code.slice(prevI, i + 2))
+      }
+      yield parseMarkdown(code.slice(i + 2, end))
+      prevI = i = end
+    } else if (c2 === '/*') {
+      const j = code.indexOf('*/', i)
+      const end = j === -1 ? N : j
+      if (i + 2 > prevI) {
+        yield new Raw(code.slice(prevI, i + 2))
+      }
+      yield parseMarkdown(code.slice(i + 2, end))
+      prevI = i = end
+    } else {
+      i++
+    }
+  }
+  if (prevI < N) {
+    yield new Raw(code.slice(prevI, N))
+  }
+}
+
+function * sql (code: string, parseMarkdown: documentParser) {
+  let i = 0; let prevI = 0
+  const N = code.length
+  while (i < N) {
+    const c2 = code.substr(i, 2)
+    if (c2 === '--') {
       const j = code.indexOf('\n', i)
       const end = j === -1 ? N : j
       if (i + 2 > prevI) {
