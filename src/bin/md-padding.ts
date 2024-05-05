af8832e291d4ecf33865616b720f3de4618f1eb4
Yang Jun <harttleharttle@gmail.com>
2024-05-06 00:19:31 +0800

    feat: support file list as input

--- src/bin/md-padding.ts
+++ src/bin/md-padding.ts
@@ -3,6 +3,8 @@ import { padMarkdown } from '../index'
 import { readFileSync, writeFileSync } from 'fs'
 import * as yargs from 'yargs'
 
+type FileList = (string | number)[]
+
 yargs
   .usage('$0 [OPTION]... <FILE>')
   .option('in-place', {
@@ -16,25 +18,34 @@ yargs
     description: 'ignore padding within/before/after these words',
     coerce: (arr) => arr.map((item: any) => String(item))
   })
+  .option('file', {
+    alias: 'f',
+    type: 'array',
+    description: 'to specify file list, used with -I',
+    coerce: (arr) => arr.map((item: any) => String(item))
+  })
   .alias('help', 'h')
   .example('stdout', 'mdp README.md')
   .example('in-place', 'mdp -i README.md')
   .example('pipe', 'cat README.md | mdp')
-  .check(argv => {
-    if (argv._.length === 0 && argv.i) {
-      throw new Error('File not specified, cannot edit in place')
-    }
-    return true
+
+const file = yargs.argv.file as (undefined | string[])
+const inputFiles: FileList = file || yargs.argv._
+if (inputFiles.length === 0 && yargs.argv.i) {
+  console.error('File not specified, cannot edit in place')
+  process.exit(1)
+}
+if (!inputFiles.length) inputFiles.push(0)  // defaults to STDIN
+
+for (const inputFile of inputFiles) {
+  const input = readFileSync(inputFile, 'utf-8')
+  const output = padMarkdown(input, {
+    ignoreWords: yargs.argv.ignoreWords as (undefined | string[])
   })
 
-const inputFile = yargs.argv._[0] || 0 // default to STDIN
-const input = readFileSync(inputFile, 'utf-8')
-const output = padMarkdown(input, {
-  ignoreWords: yargs.argv.ignoreWords as (undefined | string[])
-})
-
-if (yargs.argv.i) {
-  writeFileSync(inputFile, output)
-} else {
-  process.stdout.write(output)
+  if (yargs.argv.i) {
+    writeFileSync(inputFile, output)
+  } else {
+    process.stdout.write(output)
+  }
 }
